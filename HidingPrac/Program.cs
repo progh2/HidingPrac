aa1375e57eb50509815631c73d16c6632208b0a4
Gihun Ham <progh2@gmail.com>
2020-06-16 10:08:55 +0900

    초기 커밋.

---
+++ HidingPrac/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HidingPrac
+{
+    class Program
+    {
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("Parent!");
+            }
+            public virtual void Method2()
+            {
+                Console.WriteLine("Parent!");
+            }
+        }
+
+        class Child : Parent
+        {
+            public new string variable = "shadowing";
+            public new void Method()
+            {
+                Console.WriteLine("Child");
+            }
+            public override void Method2()
+            {
+                Console.WriteLine("Child");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+            Console.WriteLine("하이딩");
+            Console.WriteLine("Child variable :" + c.variable);
+            Console.WriteLine("Parent variable :" + ((Parent)c).variable);            
+            c.Method();
+            ((Parent)c).Method();
+            Console.WriteLine("오버라이딩");
+            c.Method2();
+            ((Parent)c).Method2();
+        }
+    }
+}
